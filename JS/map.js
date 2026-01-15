8a1d1358383aa582f3d6ba44a3255c79acfcdbeb
WesneyBraz <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>
2026-01-15 11:49:20 -0300

    start commit

---
+++ JS/map.js
@@ -0,0 +1,37 @@
+const map = L.map('map').setView([-15.793889, -47.882778], 4);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+let markupFirst = null;
+
+document.getElementById('searchBtn').addEventListener('click', async () => {
+    const query = document.getElementById('searchInput').value.trim();
+    if (!query) return;
+
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`
+    try {
+        const response = await fetch(url);
+        const results = await response.json();
+
+        if (results.length > 0) {
+            const { lat, lon, display_name } = results[0];
+
+            if (markupFirst) {
+                map.removeLayer(markupFirst);
+            }
+
+            markupFirst = L.marker([lat, lon]).addTo(map)
+                .bindPopup(display_name)
+                .openPopup();
+
+            map.setView([lat, lon], 15);
+        } else {
+            alert('Não foi possivel encontrar o local!');
+        }
+    }
+    catch (error) {
+        alert('Ocorreu um erro!');
+    }
+});
